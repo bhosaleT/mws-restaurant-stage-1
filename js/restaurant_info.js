e8ad1ba51038c653c4a3e54d0ae0906379cf9a4d
Tejas Bhosale <bhosaletejas93@gmail..com>
2018-08-28 21:05:34 +0530

    Service worker working cache stored offline working

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -11,50 +11,50 @@ document.addEventListener('DOMContentLoaded', (event) => {
 /**
  * Initialize leaflet map
  */
-// initMap = () => {
-//   fetchRestaurantFromURL((error, restaurant) => {
-//     if (error) { // Got an error!
-//       console.error(error);
-//     } else {      
-//       self.newMap = L.map('map', {
-//         center: [restaurant.latlng.lat, restaurant.latlng.lng],
-//         zoom: 16,
-//         scrollWheelZoom: false
-//       });
-//       L.tileLayer(
-//         "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}",
-//         {
-//           mapboxToken:
-//             "pk.eyJ1IjoidGVqYXNiaG9zYWxlIiwiYSI6ImNqbGF2aXVobDA3d24zdm8wd3NvNm5uOXgifQ.YunO4sVnTbSLZ9Tm9joA1A",
-//           maxZoom: 18,
-//           attribution:
-//             'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-//             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-//             'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//           id: "mapbox.streets"
-//         }
-//       ).addTo(newMap);
-//       fillBreadcrumb();
-//       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
-//     }
-//   });
-// }  
- 
- window.initMap = () => {
+initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
       console.error(error);
-    } else {
-      self.map = new google.maps.Map(document.getElementById('map'), {
+    } else {      
+      self.newMap = L.map('map', {
+        center: [restaurant.latlng.lat, restaurant.latlng.lng],
         zoom: 16,
-        center: restaurant.latlng,
-        scrollwheel: false
+        scrollWheelZoom: false
       });
+      L.tileLayer(
+        "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}",
+        {
+          mapboxToken:
+            "pk.eyJ1IjoidGVqYXNiaG9zYWxlIiwiYSI6ImNqbGF2aXVobDA3d24zdm8wd3NvNm5uOXgifQ.YunO4sVnTbSLZ9Tm9joA1A",
+          maxZoom: 18,
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          id: "mapbox.streets"
+        }
+      ).addTo(newMap);
       fillBreadcrumb();
-      DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+      DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
     }
   });
-} 
+}  
+ 
+//  window.initMap = () => {
+//   fetchRestaurantFromURL((error, restaurant) => {
+//     if (error) { // Got an error!
+//       console.error(error);
+//     } else {
+//       self.map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 16,
+//         center: restaurant.latlng,
+//         scrollwheel: false
+//       });
+//       fillBreadcrumb();
+//       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
+//     }
+//   });
+// } 
 
 /**
  * Get current restaurant from page URL.
