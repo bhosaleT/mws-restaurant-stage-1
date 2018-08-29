9a35b79d1262b13fa118107582c8e5b849def456
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-08-29 12:46:50 +0530

    fixed service worker bugs

--- sw.js
+++ sw.js
@@ -1,41 +1,56 @@
 let cacheName = "restaurant-info";
 
-self.addEventListener('install', (event) => {
-let urlsToCache = [
-    '/',
-    './index.html',
-    './restaurant.html',
-    './css/styles.css',
-    './js/dbhelper.js',
-    './js/main.js',
-    './js/restaurant_info.js',
-    './data/restaurants.json',
-    './img/1.jpg',
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-    './img/10.jpg'
-];
- event.waitUntil(
-     caches.open(cacheName).then((cache)=> {
-         console.log('cache opened');
-         return cache.addAll(urlsToCache);
-     })
- );
+self.addEventListener("install", event => {
+  let urlsToCache = [
+    "/",
+    "./index.html",
+    "./restaurant.html",
+    "./css/styles.css",
+    "./js/dbhelper.js",
+    "./js/main.js",
+    "./js/restaurant_info.js",
+    "./data/restaurants.json",
+    "./img/1.jpg",
+    "./img/2.jpg",
+    "./img/3.jpg",
+    "./img/4.jpg",
+    "./img/5.jpg",
+    "./img/6.jpg",
+    "./img/7.jpg",
+    "./img/8.jpg",
+    "./img/9.jpg",
+    "./img/10.jpg"
+  ];
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log("cache opened");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
 
+      var fetchRequest = event.request.clone();
 
-self.addEventListener('fetch', function(event){
-    event.respondWith(
-        caches.match(event.request).then((response) =>{
-            if(response) return response;
-            return fetch(event.request);
-        })
-    )
-})
+      return fetch(fetchRequest).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+          var responseToCache = response.clone();
+
+          caches.open(cacheName)
+              .then(function (cache) {
+                  cache.put(event.request, responseToCache);
+              });
+
+          return response;
+      });
+    })
+  );
+});
